fe32e81d9a3e3d6c3bc229ad0c95b0d41478d2c4
murugappankrishna <murugappankrishna@icloud.com>
2024-12-09 00:30:56 +0530

    Fix:Cross Error

--- src/main/java/com/murugappan/controller/FileMetaDataParser.java
+++ src/main/java/com/murugappan/controller/FileMetaDataParser.java
@@ -1,16 +1,19 @@
 package com.murugappan.controller;
 
 import com.murugappan.DTO.ResponseDTO;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.HashMap;
 import java.util.Map;
+@Controller
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class FileMetaDataParser {
     @PostMapping("/fileanalyse")
+    @ResponseBody
     public ResponseDTO handleFileUpload(@RequestParam("upfile") MultipartFile file) {
             ResponseDTO response = new ResponseDTO();
            response.setName(file.getOriginalFilename());
